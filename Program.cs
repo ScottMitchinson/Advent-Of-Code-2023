afa93f518d846ca2f76e0c63f4d61c37c922470d
Scott Mitchinson <scottmitchinson1903@gmail.com>
2023-12-03 12:57:11 +0000

    Added Day 3 part 1
    I just got to part 2 and wtf

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Numerics;
+using System.Text.RegularExpressions;
 
 class AdventOfCode
 {
@@ -102,10 +104,101 @@ class AdventOfCode
         Console.WriteLine(Answer);
     }
 
+    static List<char> GetSurroundingCharacters(int ColumnIndex, int RowIndex, string[] FileLines)
+    {
+        string CurrentRow = FileLines[RowIndex];
+
+        // This makes some gross assumptions about the length of every line. It also
+        // duplicates values. But for our purpose this is fine.
+        int LeftIndex   = Math.Clamp(ColumnIndex - 1, 0, CurrentRow.Length - 1);
+        int RightIndex  = Math.Clamp(ColumnIndex + 1, 0, CurrentRow.Length - 1);
+        int TopIndex    = Math.Clamp(RowIndex - 1, 0, FileLines.Length - 1);
+        int BottomIndex = Math.Clamp(RowIndex + 1, 0, FileLines.Length - 1);
+
+        List<char> SurroundingCharacters = new List<char>();
+        SurroundingCharacters.Add(FileLines[TopIndex][LeftIndex]);      // TopLeft
+        SurroundingCharacters.Add(FileLines[TopIndex][ColumnIndex]);    // TopMiddle
+        SurroundingCharacters.Add(FileLines[TopIndex][RightIndex]);     // TopRight
+        SurroundingCharacters.Add(FileLines[BottomIndex][LeftIndex]);   // BottomLeft
+        SurroundingCharacters.Add(FileLines[BottomIndex][ColumnIndex]); // BottomMiddle
+        SurroundingCharacters.Add(FileLines[BottomIndex][RightIndex]);  // BottomRight
+        SurroundingCharacters.Add(FileLines[RowIndex][LeftIndex]);      // Left
+        SurroundingCharacters.Add(FileLines[RowIndex][RightIndex]);     // Right
+
+        return SurroundingCharacters;
+    }
+
+    static void Puzzle3()
+    {
+        string[] FileLines = File.ReadAllLines("AdventOfCode2023/inputs/input3.txt");
+
+        List<int> Numbers = new List<int>();
+        string CurrentNumber = "";
+        bool bIsPartValid = false;
+
+        // This is the regex I was attempting to use at the start
+        // it did not work. Not sure why though off the top of my head
+        Regex SymbolRegex = new Regex("[*/@&$=#-+%]");
+
+        Action TryCacheNumber = () =>
+        {
+            if(CurrentNumber.Length > 0)
+            {
+                if(bIsPartValid)
+                {
+                    Numbers.Add(Int32.Parse(CurrentNumber));
+                }
+
+                CurrentNumber = "";
+                bIsPartValid = false; 
+            }
+        };
+
+        for(int RowIndex = 0; RowIndex < FileLines.Length; RowIndex++)
+        {
+            string CurrentRow = FileLines[RowIndex];
+            
+            for(int ColumnIndex = 0; ColumnIndex < FileLines[RowIndex].Length; ColumnIndex++)
+            {
+                if(Char.IsDigit(CurrentRow[ColumnIndex]))
+                {
+                    CurrentNumber += (CurrentRow[ColumnIndex]);
+
+                    List<char> SurroundingCharacters = GetSurroundingCharacters(ColumnIndex, RowIndex, FileLines);
+                    foreach(char Character in SurroundingCharacters)
+                    {
+                        // Char.IsSymbol does not match '*'???? Using a Regex instead then
+                        //if(Character != '.' && SymbolRegex.IsMatch(Character.ToString()))
+                        if(Character != '.' && !Char.IsDigit(Character))
+                        {
+                            bIsPartValid = true;
+                        }
+                    }
+
+                    continue;
+                }
+
+                TryCacheNumber();
+            }
+
+            // End of the Line, cache whatever we have
+            TryCacheNumber();
+        }
+
+        int Answer =  0;
+        foreach (int Number in Numbers)
+        {
+            Answer += Number;
+        }
+
+        Console.WriteLine(Answer);
+    }
+
     static void Main(string[] args) 
     {
         //Puzzle1();
-        Puzzle2();
+        //Puzzle2();
+        Puzzle3();
     }
 }
 
